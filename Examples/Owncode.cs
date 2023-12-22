470008bc8d7cf70f6a171957e01235352b18cef4
A_ S_ <anay0009@mail.hansenberg.dk>
2023-12-22 13:32:01 +0100

    Made my own code

--- Examples/Owncode.cs
+++ Examples/Owncode.cs
@@ -4,7 +4,28 @@ class ownCode{
 
     static void Main(){
 
+        string name;
+        int age, students;
+
+        Console.WriteLine("\nHow many student are there in your class?\n ");
+
+        Console.Write("Input the number of students: ");
+        students = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("---------------------------------------");
         
+        Console.WriteLine("Write the student's name one at a time.\n ");
+
+        for (int i = 0; i < students; i++){
+            Console.Write("Name: ");
+            name = Console.ReadLine()!;
+        
+            Console.Write("The age of that student: ");
+            age = Convert.ToInt32(Console.ReadLine()); 
+
+            Console.WriteLine("---------------------------------------");
+        }
+
     }
 
 }
