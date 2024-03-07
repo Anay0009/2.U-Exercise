693d326f3eecbffd1c3a79969dc319e71040f111
A_ S_ <anay0009@mail.hansenberg.dk>
2024-03-07 19:21:31 +0100

    Own code presentation

--- Examples/Owncode.cs
+++ Examples/Owncode.cs
@@ -3,65 +3,115 @@
 // class OwnCode
 // {
 
-//     static void Main()
-//     {
-
-//         string name;
-//         int age, students, totalAge = 0;
-
-//         Console.WriteLine("\nHow many student are there in your class?\n ");
-
-//         Console.Write("Input the number of students: ");
-//         students = Convert.ToInt32(Console.ReadLine());
-
-//         Console.WriteLine("---------------------------------------");
-        
-//         Console.WriteLine("Write the student's name one at a time.\n ");
-
-//         for (int i = 0; i < students; i++)
+//         static void Main()
 //         {
-//             Console.Write("Name: ");
-//             name = Console.ReadLine()!;
-        
-//             Console.Write("The age of that student: ");
-//             age = Convert.ToInt32(Console.ReadLine()); 
-            
-//             totalAge = totalAge + age;
 
-//             Console.WriteLine("---------------------------------------");
+//                 string name;
+//                 int age, students, totalAge = 0;
+
+//                 Console.WriteLine("\nHow many student are there in your class?\n ");
+
+//                 Console.Write("Input the number of students: ");
+//                 students = Convert.ToInt32(Console.ReadLine());
+
+//                 Console.WriteLine("---------------------------------------");
+
+//                 Console.WriteLine("Write the student's name one at a time.\n ");
+
+//                 for (int i = 0; i < students; i++)
+//                 {
+//                         Console.Write("Name: ");
+//                         name = Console.ReadLine()!;
+
+//                         Console.Write("The age of that student: ");
+//                         age = Convert.ToInt32(Console.ReadLine());
+
+//                         totalAge = totalAge + age;
+
+//                         Console.WriteLine("---------------------------------------");
+//                 }
+
+//                 double averageAge = (double)totalAge / students;
+
+//                 Console.WriteLine("\nYou've listed {0} names, indicating there are {0} students in your class.", students);
+//                 Console.WriteLine("The average age of the students is: {0}\n", averageAge);
+
+
 //         }
 
-//         double averageAge = (double)totalAge/students;
-
-//         Console.WriteLine("\nYou've listed {0} names, indicating there are {0} students in your class.", students);
-//         Console.WriteLine("The average age of the students is: {0}\n", averageAge);
-
-
-//         // Own code to a string
-
-        // string path;
-
-        // Console.WriteLine("Which way do you want to go?");
-        // Console.WriteLine("Left, Right, or Down?");
-        // path = Console.ReadLine()!.ToLower();
-
-        // switch (path)
-        // {
-        //     case "left":
-        //         Console.WriteLine("You chose to go left.");
-        //         break;
-        //     case "right":
-        //         Console.WriteLine("You chose to go right.");
-        //         break;
-        //     case "down":
-        //         Console.WriteLine("You chose to go down.");
-        //         break;
-        //     default:
-        //         Console.WriteLine("You found the hidden path.");
-        //         break;
-        // }
-
-//     }
-
 // }
-    
+
+
+
+class GuessingGame
+{
+        static void Main()
+        {
+                // Welcome message
+                Console.WriteLine("Welcome to the Guessing Game!");
+                Console.WriteLine("Try to guess the secret number between 1 and 100.");
+                Console.WriteLine("You have 10 attempts.");
+
+                // Generate a random secret number between 1 and 100
+                int secretNumber = new Random().Next(1, 101);
+
+                int maxAttempts = 10;
+                int attempts = 0; // Track the number of attempts
+
+                // Main game loop
+                while (attempts < maxAttempts)
+                {
+                        // Get the user's guess
+                        int userGuess = GetUserGuess();
+
+                        // Increment the attempts counter
+                        attempts++;
+
+                        // Check if the guess is correct
+                        if (CheckGuess(userGuess, secretNumber))
+                        {
+                                Console.WriteLine($"Congratulations! You guessed the correct number in {attempts} attempts.");
+                                break;
+                        }
+                        else
+                        {
+                                // Provide feedback on the user's guess
+                                if (IsClose(userGuess, secretNumber))
+                                {
+                                        Console.WriteLine($"Close, but not quite. You have {maxAttempts - attempts} attempts left. Try again.");
+                                }
+                                else
+                                {
+                                        Console.WriteLine($"Not even close. You have {maxAttempts - attempts} attempts left. Try again.");
+                                }
+                        }
+                }
+
+                // If the maximum attempts are reached and the loop hasn't broken, inform the user
+                if (attempts == maxAttempts)
+                {
+                        Console.WriteLine($"Sorry, you've used all {maxAttempts} attempts. The correct number was {secretNumber}.");
+                }
+        }
+
+        // Method to get the user's guess
+        static int GetUserGuess()
+        {
+                Console.Write("Enter your guess: ");
+                return Convert.ToInt32(Console.ReadLine());
+        }
+
+        // Method to check if the guess is correct
+        static bool CheckGuess(int guess, int secretNumber)
+        {
+                return guess == secretNumber;
+        }
+
+        // Method to check if the guess is close
+        static bool IsClose(int guess, int secretNumber)
+        {
+                // Define a threshold for closeness (e.g., within 10 units)
+                int threshold = 10;
+                return Math.Abs(secretNumber - guess) <= threshold;
+        }
+}
--- Examples/Recursion.cs
+++ Examples/Recursion.cs
@@ -1,167 +1,165 @@
-using System.Globalization;
+// class Recursion
+// {
 
-class Recusrion
-{
+//     static void Main()
+//     {
+//         Console.WriteLine("What exercise do you want me to find?");
+//         Console.WriteLine("1, 2, 3, 4, 6, 7, 8, 9 or 10?: ");
+//         int x = Convert.ToInt32(Console.ReadLine());
 
-    static void Main()
-    {
-        Console.WriteLine("What exercise do you want me to find?");
-        Console.WriteLine("1, 2, 3, 4, 6, 7, 8, 9 or 10?: ");
-        int x = Convert.ToInt32(Console.ReadLine());
+//         switch (x)
+//         {
+//             case 1:
+//                 Exercise1();
+//                 break;
+//             case 2:
+//                 Exercise2();
+//                 break;
+//             case 3:
+//                 Exercise3();
+//                 break;
+//             case 4:
+//                 Exercise4();
+//                 break;
+//             case 5:
+//                 Exercise5();
+//                 break;
+//                 // case 6:
+//                 //     Exercise6();
+//                 //     break;
+//                 // case 7:
+//                 //     Exercise7();
+//                 //     break;
+//                 // case 8:
+//                 //     Exercise8();
+//                 //     break;
+//                 // case 9:
+//                 //     Exercise9();
+//                 //     break;
+//                 // case 10:
+//                 //     Exercise10();
+//                 //     break;
+//         }
+//     }
 
-        switch (x)
-        {
-            case 1:
-                Exercise1();
-                break;
-            case 2:
-                Exercise2();
-                break;
-            case 3:
-                Exercise3();
-                break;
-            case 4:
-                Exercise4();
-                break;
-            case 5:
-                Exercise5();
-                break;
-                // case 6:
-                //     Exercise6();
-                //     break;
-                // case 7:
-                //     Exercise7();
-                //     break;
-                // case 8:
-                //     Exercise8();
-                //     break;
-                // case 9:
-                //     Exercise9();
-                //     break;
-                // case 10:
-                //     Exercise10();
-                //     break;
-        }
-    }
+//     // -------------------------------------------------------
+//     // Exercise 1: Write a program in C# Sharp to print the first n natural numbers using recursion.
 
-    // -------------------------------------------------------
-    // Exercise 1: Write a program in C# Sharp to print the first n natural numbers using recursion.
+//     static void Exercise1()
+//     {
+//         Console.WriteLine("---------------------------------------");
+//         Console.WriteLine(" Write a program in C# Sharp to print the first n natural numbers using recursion.");
+//         Console.WriteLine("---------------------------------------");
 
-    static void Exercise1()
-    {
-        Console.WriteLine("---------------------------------------");
-        Console.WriteLine(" Write a program in C# Sharp to print the first n natural numbers using recursion.");
-        Console.WriteLine("---------------------------------------");
+//         Rec(1, 10);
+//     }
 
-        Rec(1, 10);
-    }
+//     static void Rec(int x, int y)
+//     {
+//         if (x > y)
+//         {
+//             return;
+//         }
 
-    static void Rec(int x, int y)
-    {
-        if (x > y)
-        {
-            return;
-        }
+//         Console.Write($"{x} ");
+//         Rec(x + 1, y);
+//     }
 
-        Console.Write($"{x} ");
-        Rec(x + 1, y);
-    }
+//     // -------------------------------------------------------
+//     // Exercise 2: Write a program in C# Sharp to print numbers from n to 1 using recursion.
 
-    // -------------------------------------------------------
-    // Exercise 2: Write a program in C# Sharp to print numbers from n to 1 using recursion.
+//     static void Exercise2()
+//     {
+//         Console.WriteLine("---------------------------------------");
+//         Console.WriteLine("Write a program in C# Sharp to print numbers from n to 1 using recursion.");
+//         Console.WriteLine("---------------------------------------");
 
-    static void Exercise2()
-    {
-        Console.WriteLine("---------------------------------------");
-        Console.WriteLine("Write a program in C# Sharp to print numbers from n to 1 using recursion.");
-        Console.WriteLine("---------------------------------------");
+//         MinRec(10, 1);
+//     }
 
-        MinRec(10, 1);
-    }
+//     static void MinRec(int x, int y)
+//     {
+//         if (x < y)
+//         {
+//             return;
+//         }
 
-    static void MinRec(int x, int y)
-    {
-        if (x < y)
-        {
-            return;
-        }
+//         Console.Write($"{x} ");
+//         MinRec(x - 1, y);
 
-        Console.Write($"{x} ");
-        MinRec(x - 1, y);
+//     }
 
-    }
+//     // -------------------------------------------------------
+//     // Exercise 3: Write a program in C# Sharp to find the sum of the first n natural numbers using.
 
-    // -------------------------------------------------------
-    // Exercise 3: Write a program in C# Sharp to find the sum of the first n natural numbers using.
+//     static void Exercise3()
+//     {
+//         Console.WriteLine("---------------------------------------");
+//         Console.WriteLine("Write a program in C# Sharp to find the sum of the first n natural numbers using.");
+//         Console.WriteLine("---------------------------------------");
 
-    static void Exercise3()
-    {
-        Console.WriteLine("---------------------------------------");
-        Console.WriteLine("Write a program in C# Sharp to find the sum of the first n natural numbers using.");
-        Console.WriteLine("---------------------------------------");
+//         Console.Write(Sum(10));
 
-        Console.Write(Sum(10));
+//     }
 
-    }
+//     static int Sum(int x)
+//     {
+//         if (x == 0)
+//         {
+//             return 0;
+//         }
+//         return x + Sum(x - 1);
+//     }
 
-    static int Sum(int x)
-    {
-        if (x == 0)
-        {
-            return 0;
-        }
-        return x + Sum(x - 1);
-    }
+//     // -------------------------------------------------------
+//     // Exercise 4: Write a program in C# Sharp to display the individual digits of a given number using recursion.
 
-    // -------------------------------------------------------
-    // Exercise 4: Write a program in C# Sharp to display the individual digits of a given number using recursion.
+//     static void Exercise4()
+//     {
+//         Console.WriteLine("---------------------------------------");
+//         Console.WriteLine("Write a program in C# Sharp to display the individual digits of a given number using recursion.");
+//         Console.WriteLine("---------------------------------------");
 
-    static void Exercise4()
-    {
-        Console.WriteLine("---------------------------------------");
-        Console.WriteLine("Write a program in C# Sharp to display the individual digits of a given number using recursion.");
-        Console.WriteLine("---------------------------------------");
+//         Space(1234);
 
-        Space(1234);
+//     }
 
-    }
+//     static void Space(int n)
+//     {
+//         if (n < 10)
+//         {
+//             Console.Write($"{n} ");
+//         }
+//         else
+//         {
+//             Space(n / 10);
+//             Console.Write($"{n % 10} ");
+//         }
 
-    static void Space(int n)
-    {
-        if (n < 10)
-        {
-            Console.Write($"{n} ");
-        }
-        else
-        {
-            Space(n / 10);
-            Console.Write($"{n % 10} ");
-        }
-
-    }
+//     }
 
 
-    // -------------------------------------------------------
-    // Exercise 5: Write a program in C# Sharp to count the number of digits in a number using recursion.
+//     // -------------------------------------------------------
+//     // Exercise 5: Write a program in C# Sharp to count the number of digits in a number using recursion.
 
-    static void Exercise5()
-    {
-        Console.WriteLine("---------------------------------------");
-        Console.WriteLine("Write a program in C# Sharp to count the number of digits in a number using recursion.");
-        Console.WriteLine("---------------------------------------");
+//     static void Exercise5()
+//     {
+//         Console.WriteLine("---------------------------------------");
+//         Console.WriteLine("Write a program in C# Sharp to count the number of digits in a number using recursion.");
+//         Console.WriteLine("---------------------------------------");
 
-        Console.Write(Numbercount(12345, 0));
+//         Console.Write(Numbercount(12345, 0));
 
-    }
+//     }
 
-    static int Numbercount(int numb, int nodigits)
-    {
-        if (numb == 0)
-            return nodigits;
+//     static int Numbercount(int numb, int nodigits)
+//     {
+//         if (numb == 0)
+//             return nodigits;
 
-        return Numbercount(numb/10, ++nodigits);
-    }
-}
+//         return Numbercount(numb/10, ++nodigits);
+//     }
+// }
 
 
 
